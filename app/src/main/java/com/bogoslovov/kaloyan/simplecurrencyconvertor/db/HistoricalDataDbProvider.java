68949a7730302d32233bb386bb4684503258aaef
Kaloyan Bogoslovov <k.bogoslovov@omisoft.eu>
2017-05-02 18:53:40 +0300

    Overrode the query method of the Content Provider HistoricalDataDbProvider.java

--- app/src/main/java/com/bogoslovov/kaloyan/simplecurrencyconvertor/db/HistoricalDataDbProvider.java
+++ app/src/main/java/com/bogoslovov/kaloyan/simplecurrencyconvertor/db/HistoricalDataDbProvider.java
@@ -2,6 +2,7 @@ package com.bogoslovov.kaloyan.simplecurrencyconvertor.db;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -12,21 +13,66 @@ import android.support.annotation.Nullable;
  */
 
 public class HistoricalDataDbProvider extends ContentProvider{
+
+    private static final UriMatcher uriMatcher = buildUriMatcher();
+    private HistoricalDataDbOpenHelper db;
+
+    private static final int HISTORICAL_DATA = 100;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = HistoricalDataDbContract.CONTENT_AUTHORITY;
+        matcher.addURI(authority,HistoricalDataDbContract.PATH_HISTORICAL_DATA, HISTORICAL_DATA);
+        return matcher;
+    }
+
+
+
     @Override
     public boolean onCreate() {
-        return false;
+        db = new HistoricalDataDbOpenHelper(getContext());
+        return true;
     }
+
+
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        Cursor cursor;
+
+        switch (uriMatcher.match(uri)){
+            case HISTORICAL_DATA:
+                cursor = db.getReadableDatabase().query(
+                    HistoricalDataDbContract.HistoricalDataEntry.TABLE_NAME,
+                    projection,
+                    selection,
+                    selectionArgs,
+                    null,
+                    null,
+                    sortOrder
+                );
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(),uri);
+        return cursor;
     }
 
     @Nullable
     @Override
-    public String getType(@NonNull Uri uri) {
-        return null;
+    public String getType(Uri uri) {
+        final int match = uriMatcher.match(uri);
+
+        switch (match) {
+            case HISTORICAL_DATA:
+                return HistoricalDataDbContract.HistoricalDataEntry.CONTENT_TYPE;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
     }
 
     @Nullable
